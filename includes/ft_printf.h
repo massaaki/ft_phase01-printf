1c95b9ca8e4ce2334e0c6b8911a7e47883313284
massaaki <eu@massaaki.com>
2022-04-20 21:34:45 -0300

    feat: add example rule function

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,8 +1,12 @@
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
 # include "libft.h"
-
-void ft_printf(void);
+# include <stdio.h>
+int ft_printf(const char *input_str, ...);
+int rule_signed_int(int value);
 
 #endif
