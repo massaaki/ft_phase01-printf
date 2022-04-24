a8e1e4b3e0d1c316bc6f3e9524644774a0392b11
massaaki <eu@massaaki.com>
2022-04-23 21:18:50 -0300

    test: ensure STRING prints and return correctly value

--- tests/ft_printf_integration_tests.c
+++ tests/ft_printf_integration_tests.c
@@ -48,7 +48,7 @@ Test(integration_pt01_test02_ft_printf, success_cases_unsigned_int_return_test)
 	cr_assert(result == expected, "Expected %d but received %d", expected, result);
 }
 
-// CHAR - Return correctly value for UNSIGNED INT values
+// CHAR - Return correctly value
 Test(integration_pt01_test03_ft_printf, success_cases_char_return_test)
 {
 	unsigned int result;
@@ -56,6 +56,17 @@ Test(integration_pt01_test03_ft_printf, success_cases_char_return_test)
 
 	result = ft_printf("hello: %c%c world\n", 'a', 'b');
 	expected = printf("hello: %c%c world\n", 'a', 'b');
+	cr_assert(result == expected, "Expected %d but received %d", expected, result);
+}
+
+// STRING - Return correctly value
+Test(integration_pt01_test04_ft_printf, success_cases_string_return_test)
+{
+	unsigned int result;
+	unsigned int expected;
+
+	result = ft_printf("hello: %s world\n", "abcdefg");
+	expected = printf("hello: %s world\n", "abcdefg");
 	cr_assert(result == expected, "Expected %d but received %d", expected, result);
 }
 
@@ -105,6 +116,7 @@ Test(integration_pt02_test06_ft_printf, success_cases_with_char_args, .init = re
 	ft_printf("hello: %c world\n", letter);
 	cr_assert_stdout_eq_str("hello: a world\n", "expect print => 'hello: a world\n'");
 }
+
 // CHAR - \n
 Test(integration_pt02_test07_ft_printf, success_cases_with_special_char_args, .init = redirect_all_stdout)
 {
@@ -118,4 +130,23 @@ Test(integration_pt02_test08_ft_printf, success_cases_with_multiple_char_args, .
 {
 	ft_printf("hello: %c %c %c %c world\n", 'a', 'b', 'c', 'd');
 	cr_assert_stdout_eq_str("hello: a b c d world\n", "expect print => 'hello: a b c d world\n'");
+}
+
+// STRING - one arg
+Test(integration_pt02_test09_ft_printf, success_cases_with_string_args, .init = redirect_all_stdout)
+{
+	char str[] = "abcdef";
+
+	ft_printf("hello: %s world\n", str);
+	cr_assert_stdout_eq_str("hello: abcdef world\n", "expect print => 'hello: abcdef world\n'");
+}
+
+// STRING - multiple args
+Test(integration_pt02_test10_ft_printf, success_cases_with_multiple_string_args, .init = redirect_all_stdout)
+{
+	char str[] = "abcdef";
+	char str2[] = "ghij";
+
+	ft_printf("hello: %s world%s\n", str, str2);
+	cr_assert_stdout_eq_str("hello: abcdef worldghij\n", "expect print => 'hello: abcdef world\n'");
 }
