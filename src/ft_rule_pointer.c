9b8faae0dcd7df73a247d806fa29c6d767a429d5
Massaaaki <eu@massaaki.com>
2022-04-27 02:34:55 +0200

    fix: return nil when null is found

--- src/ft_rule_pointer.c
+++ src/ft_rule_pointer.c
@@ -5,6 +5,8 @@ unsigned int ft_rule_pointer(unsigned long long arg)
 	char *result;
 	unsigned int length;
 
+	if (arg == 0)
+		return (write(1, "(nil)", 5));
 	result = ft_llong_i_to_hex(arg);
 	length = write(1, result, ft_strlen(result));
 	free(result);
